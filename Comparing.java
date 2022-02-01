2be0554b6d47df059bf6e817dc46a32b3828bee2
MattUehling <98789889+MattUehling@users.noreply.github.com>
2022-02-01 12:09:19 -0600

    Update Comparing.java

--- Comparing.java
+++ Comparing.java
@@ -1,13 +1,12 @@
 import java.util.ArrayList;
 public class Comparing
 {
-  private ArrayList<String> Cities = new ArrayList<String>();
-  public String selectedLocation;
+  
+  private String selectedLocation="Other";
+  
   public boolean Cities(String location)
   {
     System.out.println(location);
-    selectedLocation = location;
-    System.out.println(selectedLocation + "----");
     if(location.equals("Omaha")||location.equals("Lincoln")||location.equals("Fremont")||location.equals("Other"))
     {
     return true;
@@ -16,16 +15,17 @@ public class Comparing
   }
   public boolean RemoveByCity(String location)
   {
-    //Why won't it work ill try later, won't save the selected location
-    System.out.println(selectedLocation +"----");
+    if(selectedLocation.equals("Other"))
+    {
+      if(!location.equals("Omaha")&& !location.equals("Lincoln")&&!location.equals("Fremont"))
+      {
+        return true;
+      }
+    }
     if(location.equals(selectedLocation))
     {
-      System.out.println("True");
       return true;
     }
-   
-
-    System.out.println("False");
      return false;
   }
 
