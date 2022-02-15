11fe0c739539afc33da303d07c99679f52dfec1b
MattUehling <98789889+MattUehling@users.noreply.github.com>
2022-02-15 09:21:37 -0600

    Update Attractions.java

--- Attractions.java
+++ Attractions.java
@@ -2,63 +2,65 @@ import java.util.Arrays;
 public class Attractions
 {
   //Giant 2D array with strings, first row is the name of the attraction and the following rows are the attributes
-  //column 1: name, column 2: city, column 3: indoor/outdoor, column 4: price range(0-5,6-10,10-20,20+)
+  //column 1: name, column 2: city, column 3: indoor/outdoor, column 4: price range(0-5,6-10,10-20,20+) 5:Wheel Chair Accesible 6: Public Transportaion/ walking distance 7:
   private String locationEntered ="";
   private String PriceRange = "";
   private String SeasonEntry="";
+  private String hAccessible="";
+  private String pubTrains = "";
   private String[][] AttractionOptions = {
-    {"Henry Doorly Zoo and Aqaurium","Omaha","Outside","20+"},
-    {"Old Market", "Omaha","Outside","0-5"},//Farmer's Market
-    { "Strategic Air and Space Museum", "Ashland","Inside","10-20"},//all indoors
-    {"Chimney Rock Historical Site","Morrill County","Outside","6-10"},
-    { "Haymarket District", "Lincoln","Outside","0-5"},
-    {"Scotts Bluff National Monument", "Gering","Outside","0-5"},
-    { "Golden Spike Tower", "North Platte","Outside","6-10"},
-    {"Indian Cave State Park", "Shubert","Outside","6-10"},
-    {"Carhenge","Alliance","Outside","0-5"},
-    {"Scenic Driving Tour of the Sandhills","Grand Island","Outside","0-5"},
-    {"Ride the Cowboy Trail", "Omaha","Outside","0-5"},
-    {"Archway", "GrandPlatte","10-20"},
-    {"Sandhill Crane Migration","Gibbon","Outside","20+"},              
-    {"Durham Museum","Omaha","Inside","10-20"},
-    {"First National's Spirit of Nebraksa's Wildreness and Pioneer Courage Park","Omaha","Outside","0-5"},
-    {"Joslyn Art Museum","Omaha","Inside","0-5"},
-    {"Performing Arts Center","Omaha","Inside","20+"},
-    {"Lauritzen Gardens" ,"Omaha","Inside","0-5"},
-    {"Omaha Children's Museum","Omaha","Inside","10-20"},
-    {"Bob Kerrey Pedrestrian Bridge","Omaha","Outside","0-5"},
-    {"Saint Cecilia Cathedral","Omaha","Inside","0-5"},
-    {"Lewis & Clark Landing","Omaha","Outside","6-10"},
-    {"Trains at Kenefick Park","Omaha","Outside","0-5"},
-    {"Freedom Park Navy Musuem", "Omaha","Inside","0-5"},
-    {"Lee G. Simmons Conservation Park & Wildlife Safari","Omaha","Outside","6-10"},
-    {"Mormon Trail Center","Omaha","Inside","0-5"},
-    { "Malcolm X House Site","Omaha","Inside","0-5"},
-    {"Tour the Capitol","Lincoln","Outside","0-5"},
-    {"UNL State Museum", "Lincoln","Inside","6-10"},
-    {"Memorial Stadium Game","Lincoln","Outside","20+"},
-    { "Sunken Gardens","Lincoln","Inside","0-5"},
-    {"Museum of American Speed","Lincoln","Inside","10-20"},
-    { "Pioneers Park Nature Center","Lincoln","Outside","0-5"},
-    {"Sheldon Musuem of Art", "Lincoln","Inside","0-5"},
-    { "Lincolns Children's Zoo","Lincoln","Outside","10-20"},
-    {"Lincolns Children Museum","Lincoln","Inside","10-20"},
-    {"International Quilt Study Center & Musuem","Lincoln","Inside","6-10"},
-    {"Governor's Residence","Lincoln","Inside","0-5"},
-    {"National Roller Skating Museum","Lincoln","Inside"},
-    {"Lester F. Larsen Tractor Test and Power Museum","Lincoln","Inside","0-5"},
-    {"CourtHouse and Jail Rocks","West Camp Clarke","Outside","0-5"},
-    { "Toadstool Geological Park","Harrison","Outside","0-5"},
-    {"Chadron State Park","Chadron","Outside","6-10"},
-    {"Agate Fossil Beds National Monument", "Whistle Creek","Outside","0-5"},
-    {"Snake River Falls", "Valentine","Outside","0-5"},
-    {"Coyote Hills Regional Park","Fremont","Outside","0-5"},
-    {"Mission Peak Regional Preserve","Fremont","Outside","0-5"},
-    {"Fremont Splash Station","Fremont","Outside","6-10"},
-    {"Ardenwood Historical Farm","Fremont","Outside",""},
-    { "Quarry Lakes Regional Recreation Area","Fremont","Outside",},
-    { "Niles Canyon Railway","Fremont","Outside",},
-    {"Old Mission San Jose","Fremont","Outside",}
+    {"Henry Doorly Zoo and Aqaurium","Omaha","Outside","20+","Yes", "Yes"},
+    {"Old Market", "Omaha","Outside","0-5", "Yes", "Yes"},//Farmer's Market
+    { "Strategic Air and Space Museum", "Ashland","Inside","10-20","Yes", "No"},//all indoors
+    {"Chimney Rock Historical Site","Morrill County","Outside","6-10","No", "No"},
+    { "Haymarket District", "Lincoln","Outside","0-5","Yes", "Yes"},
+    {"Scotts Bluff National Monument", "Gering","Outside","0-5","Yes", "No"},
+    { "Golden Spike Tower", "North Platte","Outside","6-10","Yes", "No"},
+    {"Indian Cave State Park", "Shubert","Outside","6-10","No", "No"},
+    {"Carhenge","Alliance","Outside","0-5","No", "No"},
+    {"Scenic Driving Tour of the Sandhills","Grand Island","Outside","0-5","Yes", "No"},
+    {"Ride the Cowboy Trail", "Omaha","Outside","0-5","Yes", "Yes"},
+    {"Archway", "Grand Platte","Outside", "10-20","Yes", "No"},
+    {"Sandhill Crane Migration","Gibbon","Outside","20+","Yes", "No"},              
+    {"Durham Museum","Omaha","Inside","10-20","Yes", "Yes"},
+    {"First National's Spirit of Nebraksa's Wildreness and Pioneer Courage Park","Omaha","Outside","0-5","Yes", "Yes"},
+    {"Joslyn Art Museum","Omaha","Inside","0-5","Yes", "Yes"},
+    {"Performing Arts Center","Omaha","Inside","20+","Yes", "Yes"},
+    {"Lauritzen Gardens" ,"Omaha","Inside","0-5","Yes","No"},
+    {"Omaha Children's Museum","Omaha","Inside","10-20","Yes","Yes"},
+    {"Bob Kerrey Pedrestrian Bridge","Omaha","Outside","0-5","Yes","No"},
+    {"Saint Cecilia Cathedral","Omaha","Inside","0-5","Yes", "Yes"},
+    {"Lewis & Clark Landing","Omaha","Outside","6-10","No","No"},
+    {"Trains at Kenefick Park","Omaha","Outside","0-5","Yes", "No"},
+    {"Freedom Park Navy Musuem", "Omaha","Inside","0-5","Yes", "Yes"},
+    {"Lee G. Simmons Conservation Park & Wildlife Safari","Omaha","Outside","6-10","No", "No"},
+    {"Mormon Trail Center","Omaha","Inside","0-5","Yes", "Yes"},
+    { "Malcolm X House Site","Omaha","Inside","0-5","Yes", "No"},
+    {"Tour the Capitol","Lincoln","Outside","0-5","Yes","Yes"},
+    {"UNL State Museum", "Lincoln","Inside","6-10","Yes", "Yes"},
+    {"Memorial Stadium Game","Lincoln","Outside","20+","Yes", "Yes"},
+    { "Sunken Gardens","Lincoln","Inside","0-5","Yes", "Yes"},
+    {"Museum of American Speed","Lincoln","Inside","10-20","Yes","Yes"},
+    { "Pioneers Park Nature Center","Lincoln","Outside","0-5","Yes", "Yes"},
+    {"Sheldon Musuem of Art", "Lincoln","Inside","0-5","Yes", "Yes"},
+    { "Lincolns Children's Zoo","Lincoln","Outside","10-20","Yes", "Yes"},
+    {"Lincolns Children Museum","Lincoln","Inside","10-20","Yes", "Yes"},
+    {"International Quilt Study Center & Musuem","Lincoln","Inside","6-10","Yes", "Yes"},
+    {"Governor's Residence","Lincoln","Inside","0-5","Yes","No"},
+    {"National Roller Skating Museum","Lincoln","Inside","0-5","Yes", "Yes"},
+    {"Lester F. Larsen Tractor Test and Power Museum","Lincoln","Inside","0-5","Yes", "Yes"},
+    {"CourtHouse and Jail Rocks","West Camp Clarke","Outside","0-5","No", "No"},
+    { "Toadstool Geological Park","Harrison","Outside","0-5","Yes", "No"},
+    {"Chadron State Park","Chadron","Outside","6-10","Yes","No"},
+    {"Agate Fossil Beds National Monument", "Whistle Creek","Outside","0-5","No", "No"},
+    {"Snake River Falls", "Valentine","Outside","0-5","No","No"},
+    {"Fremont Lakes State Recreation Area","Fremont","Outside","6-10","Yes", "No"},
+    {"Bryson's Airboat Tours","Fremont","Outside","20+","Yes", "No"},
+    {"Fremont Splash Station","Fremont","Outside","6-10","Yes", "No"},
+    {"Tin Lizzy Tavern","Fremont","Inside","0-5","Yes", "Yes"},
+    { "Louis E. May Museum","Fremont","Inside","0-5","Yes","No"},
+    { "Ruff House","Fremont","Inside","20+","Yes","No"},
+    {"Thirty Bowl","Fremont","Inside","10-20","Yes","No"}
   };
   public void enteredLocation(String location)
   {
@@ -72,7 +74,14 @@ public class Attractions
   {
     SeasonEntry = season;
   }
-
+  public void enteredHandicap(String h)
+  {
+    hAccessible = h;
+  }
+  public void enteredPTransportation(String Trains)
+  {
+    pubTrains = Trains;
+  }
   public String toString(String k)
   {
     return " "+k;
@@ -112,21 +121,59 @@ public class Attractions
         }
     }
   }
+  
+  public void testHandicap()
+  {
+    for(int row=0;row<AttractionOptions.length;row++)
+    {
+      if(Compare.RemoveByHandicap(AttractionOptions[row][4],hAccessible)==true)
+      {
+        System.out.println(AttractionOptions[row][0]);
+      }
+    }
+  }
+public void testPubTransport()
+  {
+    int count=0;
+    for(int row=0;row<AttractionOptions.length;row++)
+    {
+      if(Compare.RemoveByTransportation(AttractionOptions[row][5],pubTrains)==true)
+      {
+        System.out.println(AttractionOptions[row][0]);
+        System.out.println(count++);
+      }
+    }
+  }
+  
   public void UltimatePrint()
   {
     int insureReturn = 0;
     for(int row =0; row<AttractionOptions.length; row++)
     {
       if(Compare.RemoveByCity(AttractionOptions[row][1],locationEntered)==true)
+      {
+        if(Compare.RemoveByInsideOrOutside(AttractionOptions[row][2],SeasonEntry)==true)
         {
-          if(Compare.RemoveByInsideOrOutside(AttractionOptions[row][2],SeasonEntry)==true)
-        {
-          System.out.println(AttractionOptions[row][0]);
-          insureReturn =1;
+          if(Compare.RemoveByPrice(AttractionOptions[row][3],PriceRange)==true)
+          {
+            if(Compare.RemoveByHandicap(AttractionOptions[row][4],hAccessible)==true)
+            {
+               if(Compare.RemoveByTransportation(AttractionOptions[row][5],pubTrains)==true)
+              {
+                System.out.println(AttractionOptions[row][0]);
+              insureReturn =1;
+              }
+              
+            }
+          }
         }
-        }
+      }
     }
     if (insureReturn == 0)
       System.out.print("Stop being picky we live in Nebraska");
+      else
+      {
+        System.out.println("Those are our recommendations for your visit, have fun!!");
+      }
   }
 }
