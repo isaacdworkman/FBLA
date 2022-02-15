02578b092fb0fe3c17b706b26e3539f442dadf84
MattUehling <98789889+MattUehling@users.noreply.github.com>
2022-02-15 09:20:24 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -15,6 +15,8 @@ class Main {
     String location="";
     String priceRange = "";
     String season = "";
+    String handicap="";
+    String pubTransport = "";
 
     //Introducotry Statement
     System.out.println("Welcome to Elkhorn we have fantastic attractions for anything that interests you");
@@ -29,37 +31,62 @@ class Main {
     }while(Compare.Cities(location)==false);//Repeating until a city is selected
     }
     Attract.enteredLocation(location);// saves the location to the Attraction class
-    Attract.testCities();//Prints out the current available cities, delete line later
+    // Attract.testCities();//Prints out the current available cities, delete line later
 
      //Season
-    System.out.println("Please enter a season: 'Spring', 'Summer', 'Fall', or 'Winter', 'Any'");
+    System.out.println("Please enter either: 'Inside', 'Outside', or 'Any'");
     season = scan.nextLine();//stores the entered season
-    if(Compare.Season(season)==false)//If the entered season can't be recongnized
+    if(Compare.InsideOut(season)==false)//If the entered season can't be recongnized
     {
     do{
     System.out.println("Sorry your selection was not recongnized please try again");
-    System.out.println("Please enter a season: 'Spring', 'Summer', 'Fall', or 'Winter', 'Any'");
+    System.out.println("Please enter either: 'Inside', 'Outside', or 'Any'");
     season = scan.nextLine();
-    }while(Compare.Season(season)==false);//Repeating until a season is selected
+    }while(Compare.InsideOut(season)==false);//Repeating until a season is selected
     }
     Attract.enteredSeason(season);// saves the Season to the Attraction class
-    Attract.testSeason();
+    // Attract.testSeason();
 
 
     //Guess i'll just do price
-    System.out.println("Please enter the price range either : 'Free', 'Under 25' or 'Above 50', 'Any'");
+    System.out.println("Please enter the price range per person, either : '0-5', '6-10' , '10-20', '20+', 'Any'");
     priceRange = scan.nextLine();//stores the entered price
     if(Compare.price(priceRange)==false)//If the entered price can't be recongnized
     {
     do{
     System.out.println("Sorry your selection was not recongnized please try again");
-    System.out.println("Please enter the price range either : 'Free', 'Under 25' or 'Above 50', 'Any'");
+    System.out.println("Please enter the price range per person, either : '0-5', '6-10' , '10-20', '20+', 'Any");
     priceRange = scan.nextLine();
     }while(Compare.price(priceRange)==false);//Repeating until a price range is selected
     }
-    Attract.enteredPrice(priceRange);// saves the price to the Attraction class
-    Attract.testPrice();
+    Attract.enteredPriceRange(priceRange);// saves the price to the Attraction class
 
-   
+    System.out.println("Would you like your desination to be handicap accessible, either: 'Yes', 'No', 'Any'");
+    handicap=scan.nextLine();
+    if(Compare.Handi(handicap)==false)
+    {
+      do{
+        System.out.println("Sorry your selection was not recongnized please try again");
+        System.out.println("Would you like your desination to be handicap accessible, either: 'Yes', 'No','Any'");handicap=scan.nextLine();
+      }while(Compare.Handi(handicap)==false);
+    }
+    Attract.enteredHandicap(handicap);
+    // Attract.testHandicap();
+
+     System.out.println("Do you need public transportation 'Yes', 'No'");
+    pubTransport=scan.nextLine();
+    if(Compare.transportation(pubTransport)==false)
+    {
+      do{
+        System.out.println("Sorry your selection was not recongnized please try again");
+        System.out.println("Do you need public transportation 'Yes', 'No'");
+        pubTransport=scan.nextLine();
+      }while(Compare.transportation(pubTransport)==false);
+    }
+    Attract.enteredPTransportation(pubTransport);
+    // Attract.testPubTransport();
+    
+
+    Attract.UltimatePrint();
   }
 }
