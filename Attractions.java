bcc0bd53a491b1b0ad6ad5c8f335ef4769f7c509
MattUehling <98789889+MattUehling@users.noreply.github.com>
2022-02-16 11:08:22 -0600

    Update Attractions.java

--- Attractions.java
+++ Attractions.java
@@ -1,67 +1,71 @@
-import java.util.Arrays;
+import java.util.Arrays;//allows for the usage of 2d arrays and array methods
 public class Attractions
 {
-  //Giant 2D array with strings, first row is the name of the attraction and the following rows are the attributes
-  //column 1: name, column 2: city, column 3: indoor/outdoor, column 4: price range(0-5,6-10,10-20,20+) 5:Wheel Chair Accesible 6: Public Transportaion/ walking distance 7:
+  //Giant 2D array with strings, first column is the name of the attraction and the following rows are the attributes; each row is a different location
+  //column 1: name, column 2: city, column 3: indoor/outdoor, column 4: price range(0-5,6-10,10-20,20+) 5:Wheel Chair Accesible 6: Public Transportaion/ walking distance 7: Requires phyiscal activity 8: Ranking out of 10, 9: Pogrammers recommendation
   private String locationEntered ="";
   private String PriceRange = "";
   private String SeasonEntry="";
   private String hAccessible="";
   private String pubTrains = "";
+  private String physical ="";
+  private String rating = "";
+  private String opinion ="";
   private String[][] AttractionOptions = {
-    {"Henry Doorly Zoo and Aqaurium","Omaha","Outside","20+","Yes", "Yes"},
-    {"Old Market", "Omaha","Outside","0-5", "Yes", "Yes"},//Farmer's Market
-    { "Strategic Air and Space Museum", "Ashland","Inside","10-20","Yes", "No"},//all indoors
-    {"Chimney Rock Historical Site","Morrill County","Outside","6-10","No", "No"},
-    { "Haymarket District", "Lincoln","Outside","0-5","Yes", "Yes"},
-    {"Scotts Bluff National Monument", "Gering","Outside","0-5","Yes", "No"},
-    { "Golden Spike Tower", "North Platte","Outside","6-10","Yes", "No"},
-    {"Indian Cave State Park", "Shubert","Outside","6-10","No", "No"},
-    {"Carhenge","Alliance","Outside","0-5","No", "No"},
-    {"Scenic Driving Tour of the Sandhills","Grand Island","Outside","0-5","Yes", "No"},
-    {"Ride the Cowboy Trail", "Omaha","Outside","0-5","Yes", "Yes"},
-    {"Archway", "Grand Platte","Outside", "10-20","Yes", "No"},
-    {"Sandhill Crane Migration","Gibbon","Outside","20+","Yes", "No"},              
-    {"Durham Museum","Omaha","Inside","10-20","Yes", "Yes"},
-    {"First National's Spirit of Nebraksa's Wildreness and Pioneer Courage Park","Omaha","Outside","0-5","Yes", "Yes"},
-    {"Joslyn Art Museum","Omaha","Inside","0-5","Yes", "Yes"},
-    {"Performing Arts Center","Omaha","Inside","20+","Yes", "Yes"},
-    {"Lauritzen Gardens" ,"Omaha","Inside","0-5","Yes","No"},
-    {"Omaha Children's Museum","Omaha","Inside","10-20","Yes","Yes"},
-    {"Bob Kerrey Pedrestrian Bridge","Omaha","Outside","0-5","Yes","No"},
-    {"Saint Cecilia Cathedral","Omaha","Inside","0-5","Yes", "Yes"},
-    {"Lewis & Clark Landing","Omaha","Outside","6-10","No","No"},
-    {"Trains at Kenefick Park","Omaha","Outside","0-5","Yes", "No"},
-    {"Freedom Park Navy Musuem", "Omaha","Inside","0-5","Yes", "Yes"},
-    {"Lee G. Simmons Conservation Park & Wildlife Safari","Omaha","Outside","6-10","No", "No"},
-    {"Mormon Trail Center","Omaha","Inside","0-5","Yes", "Yes"},
-    { "Malcolm X House Site","Omaha","Inside","0-5","Yes", "No"},
-    {"Tour the Capitol","Lincoln","Outside","0-5","Yes","Yes"},
-    {"UNL State Museum", "Lincoln","Inside","6-10","Yes", "Yes"},
-    {"Memorial Stadium Game","Lincoln","Outside","20+","Yes", "Yes"},
-    { "Sunken Gardens","Lincoln","Inside","0-5","Yes", "Yes"},
-    {"Museum of American Speed","Lincoln","Inside","10-20","Yes","Yes"},
-    { "Pioneers Park Nature Center","Lincoln","Outside","0-5","Yes", "Yes"},
-    {"Sheldon Musuem of Art", "Lincoln","Inside","0-5","Yes", "Yes"},
-    { "Lincolns Children's Zoo","Lincoln","Outside","10-20","Yes", "Yes"},
-    {"Lincolns Children Museum","Lincoln","Inside","10-20","Yes", "Yes"},
-    {"International Quilt Study Center & Musuem","Lincoln","Inside","6-10","Yes", "Yes"},
-    {"Governor's Residence","Lincoln","Inside","0-5","Yes","No"},
-    {"National Roller Skating Museum","Lincoln","Inside","0-5","Yes", "Yes"},
-    {"Lester F. Larsen Tractor Test and Power Museum","Lincoln","Inside","0-5","Yes", "Yes"},
-    {"CourtHouse and Jail Rocks","West Camp Clarke","Outside","0-5","No", "No"},
-    { "Toadstool Geological Park","Harrison","Outside","0-5","Yes", "No"},
-    {"Chadron State Park","Chadron","Outside","6-10","Yes","No"},
-    {"Agate Fossil Beds National Monument", "Whistle Creek","Outside","0-5","No", "No"},
-    {"Snake River Falls", "Valentine","Outside","0-5","No","No"},
-    {"Fremont Lakes State Recreation Area","Fremont","Outside","6-10","Yes", "No"},
-    {"Bryson's Airboat Tours","Fremont","Outside","20+","Yes", "No"},
-    {"Fremont Splash Station","Fremont","Outside","6-10","Yes", "No"},
-    {"Tin Lizzy Tavern","Fremont","Inside","0-5","Yes", "Yes"},
-    { "Louis E. May Museum","Fremont","Inside","0-5","Yes","No"},
-    { "Ruff House","Fremont","Inside","20+","Yes","No"},
-    {"Thirty Bowl","Fremont","Inside","10-20","Yes","No"}
+    {"Henry Doorly Zoo and Aqaurium","Omaha","Outside","20+","Yes", "Yes","Yes","4.5","Yes"},
+    {"Old Market", "Omaha","Outside","0-5", "Yes", "Yes","No","4.5","No"},//Farmer's Market
+    { "Strategic Air and Space Museum", "Ashland","Inside","10-20","Yes", "No","No", "4.7","Yes"},//all indoors
+    {"Chimney Rock Historical Site","Morrill County","Outside","6-10","No", "No","No","4.4","Yes"},
+    { "Haymarket District", "Lincoln","Outside","0-5","Yes", "Yes","No", "4.7","No"},
+    {"Scotts Bluff National Monument", "Gering","Outside","0-5","Yes", "No","Yes", "4.8","No"},
+    { "Golden Spike Tower", "North Platte","Outside","6-10","Yes", "No","No", "4.7","Yes"},
+    {"Indian Cave State Park", "Shubert","Outside","6-10","No", "No","Yes", "4.5","Yes"},
+    {"Carhenge","Alliance","Outside","0-5","No", "No","No", "4.6","Yes"},
+    {"Scenic Driving Tour of the Sandhills","Grand Island","Outside","0-5","Yes", "No","No", "4.5","No"},
+    {"Ride the Cowboy Trail", "Omaha","Outside","0-5","Yes", "Yes","Yes", "4.5","No"},
+    {"Archway", "Grand Platte","Outside", "10-20","Yes", "No","No","4.6","No"},
+    {"Sandhill Crane Migration","Gibbon","Outside","20+","Yes", "No","No","5","Yes","No"},              
+    {"Durham Museum","Omaha","Inside","10-20","Yes", "Yes","No","4.7","Yes"},
+    {"First National's Spirit of Nebraksa's Wildreness and Pioneer Courage Park","Omaha","Outside","0-5","Yes", "Yes","No","4.4","Yes"},
+    {"Joslyn Art Museum","Omaha","Inside","0-5","Yes", "Yes","No","4.7","No"},
+    {"Performing Arts Center","Omaha","Inside","20+","Yes", "Yes","No", "4.8","No"},
+    {"Lauritzen Gardens" ,"Omaha","Inside","0-5","Yes","No","No", "4.8","No"},
+    {"Omaha Children's Museum","Omaha","Inside","10-20","Yes","Yes","Yes", "4.7","No"},
+    {"Bob Kerrey Pedrestrian Bridge","Omaha","Outside","0-5","Yes","No","Yes", "4.7","Yes"},
+    {"Saint Cecilia Cathedral","Omaha","Inside","0-5","Yes", "Yes","No", "4.7","Yes"},
+    {"Lewis & Clark Landing","Omaha","Outside","6-10","No","No","Yes", "4.6","Yes"},
+    {"Trains at Kenefick Park","Omaha","Outside","0-5","Yes", "No","No", "4.8","Yes"},
+    {"Freedom Park Navy Musuem", "Omaha","Inside","0-5","Yes", "Yes","No", "4.4","Yes"},
+    {"Lee G. Simmons Conservation Park & Wildlife Safari","Omaha","Outside","6-10","No", "No","No", "4.7","Yes"},
+    {"Mormon Trail Center","Omaha","Inside","0-5","Yes", "Yes","No","4.8","No"},
+    { "Malcolm X House Site","Omaha","Inside","0-5","Yes", "No","No","4.5","Yes"},
+    {"Tour the Capitol","Lincoln","Outside","0-5","Yes","Yes","Yes","5","No"},
+    {"UNL State Museum", "Lincoln","Inside","6-10","Yes", "Yes","No","4.7","Yes"},
+    {"Memorial Stadium Game","Lincoln","Outside","20+","Yes", "Yes","No", "4.8","Yes"},
+    {"Sunken Gardens","Lincoln","Inside","0-5","Yes", "Yes","Yes", "4.8","No"},
+    {"Museum of American Speed","Lincoln","Inside","10-20","Yes","Yes","No", "4.8","Yes"},
+    { "Pioneers Park Nature Center","Lincoln","Outside","0-5","Yes", "Yes","No", "4.7","Yes"},
+    {"Sheldon Musuem of Art", "Lincoln","Inside","0-5","Yes", "Yes","No", "4.5","No"},
+    { "Lincolns Children's Zoo","Lincoln","Outside","10-20","Yes", "Yes","Yes","4.7","No"},
+    {"Lincolns Children Museum","Lincoln","Inside","10-20","Yes", "Yes","Yes", "4.7","No"},
+    {"International Quilt Study Center & Musuem","Lincoln","Inside","6-10","Yes", "Yes","No","4.6","No"},
+    {"Governor's Residence","Lincoln","Inside","0-5","Yes","No","No","5","Yes"},
+    {"National Roller Skating Museum","Lincoln","Inside","0-5","Yes", "Yes","No", "4.3","No"},
+    {"Lester F. Larsen Tractor Test and Power Museum","Lincoln","Inside","0-5","Yes", "Yes","No", "4.6","Yes"},
+    {"CourtHouse and Jail Rocks","West Camp Clarke","Outside","0-5","No", "No","Yes", "4.7","Yes"},
+    { "Toadstool Geological Park","Harrison","Outside","0-5","Yes", "No","Yes", "4.7","Yes"},
+    {"Chadron State Park","Chadron","Outside","6-10","Yes","No","Yes","4.7","Yes"},
+    {"Agate Fossil Beds National Monument", "Whistle Creek","Outside","0-5","No", "No","Yes", "4.5","Yes"},
+    {"Snake River Falls", "Valentine","Outside","0-5","No","No","Yes","4","No"},
+    {"Fremont Lakes State Recreation Area","Fremont","Outside","6-10","Yes", "No","Yes", "4.4","Yes"},
+    {"Bryson's Airboat Tours","Fremont","Outside","20+","Yes", "No","No","5","Yes"},
+    {"Fremont Splash Station","Fremont","Outside","6-10","Yes", "No", "Yes", "4.4","Yes"},
+    {"Tin Lizzy Tavern","Fremont","Inside","0-5","Yes", "Yes","No","4.3","No"},
+    { "Louis E. May Museum","Fremont","Inside","0-5","Yes","No","No","4.8","No"},
+    { "Ruff House","Fremont","Inside","20+","Yes","No","Yes","4.3","No"},
+    {"Thirty Bowl","Fremont","Inside","10-20","Yes","No","Yes","4.4","No"}
   };
+  //each method below stores the users input/preference to the associated attribute variable
   public void enteredLocation(String location)
   {
     locationEntered =location;
@@ -82,72 +86,30 @@ public class Attractions
   {
     pubTrains = Trains;
   }
+  public void enteredPhysical(String p)
+  {
+    physical=p;
+  }
+  public void enteredRating(String rate)
+  {
+    rating = rate;
+  }
+  public void enteredOpinion(String opin)
+  {
+    opinion  =opin;
+  }
+  //toString method to modify the normal System.out.print method
   public String toString(String k)
   {
     return " "+k;
   }
-  Comparing Compare = new Comparing();
- 
-  public void testCities()
-  {
-    for(int row =0; row<AttractionOptions.length; row++)
-    {
-      if(Compare.RemoveByCity(AttractionOptions[row][1],locationEntered)==true)
-        {
-          System.out.println(AttractionOptions[row][0]);
-        }
-    }
-  }
 
-   public void testSeason()
-  {
-    for(int row =0; row<AttractionOptions.length; row++)
-    {
-      if(Compare.RemoveByInsideOrOutside(AttractionOptions[row][2],SeasonEntry)==true)
-        {
-          System.out.println(AttractionOptions[row][0]);
-          
-        }
-      
-    }
-  }
-   public void testPrice()
-  {
-    for(int row =0; row<AttractionOptions.length; row++)
-    {
-      if(Compare.RemoveByPrice(AttractionOptions[row][3],PriceRange)==true)
-        {
-          System.out.println(AttractionOptions[row][0]);
-        }
-    }
-  }
-  
-  public void testHandicap()
-  {
-    for(int row=0;row<AttractionOptions.length;row++)
-    {
-      if(Compare.RemoveByHandicap(AttractionOptions[row][4],hAccessible)==true)
-      {
-        System.out.println(AttractionOptions[row][0]);
-      }
-    }
-  }
-public void testPubTransport()
-  {
-    int count=0;
-    for(int row=0;row<AttractionOptions.length;row++)
-    {
-      if(Compare.RemoveByTransportation(AttractionOptions[row][5],pubTrains)==true)
-      {
-        System.out.println(AttractionOptions[row][0]);
-        System.out.println(count++);
-      }
-    }
-  }
-  
-  public void UltimatePrint()
+  Comparing Compare = new Comparing();//creates an obejct of the comparing class to call comapring methods
+ 
+  public void UltimatePrint()//returns the final list of location options that match the users preferences
   {
     int insureReturn = 0;
+    //each compare method below runs through each attraction's specified attribute for that column to see if it matches the users preference, and if it doesn't, that location is removed from the final output of recommended places to visit
     for(int row =0; row<AttractionOptions.length; row++)
     {
       if(Compare.RemoveByCity(AttractionOptions[row][1],locationEntered)==true)
@@ -160,8 +122,18 @@ public void testPubTransport()
             {
                if(Compare.RemoveByTransportation(AttractionOptions[row][5],pubTrains)==true)
               {
-                System.out.println(AttractionOptions[row][0]);
-              insureReturn =1;
+                if(Compare.RemoveByPysicalActivity(AttractionOptions[row][6],physical)==true)
+                {
+                  if(Compare.RemoveByRating(AttractionOptions[row][7],rating)==true)
+                  {
+                    if(Compare.RemoveByOpinion(AttractionOptions[row][8],opinion)==true)
+                    {
+                    System.out.println(AttractionOptions[row][0]);
+                    insureReturn =1;
+                    }
+                  }
+                }
+                
               }
               
             }
@@ -170,10 +142,10 @@ public void testPubTransport()
       }
     }
     if (insureReturn == 0)
-      System.out.print("Stop being picky we live in Nebraska");
+      System.out.print("Sorry, no attractions matched your travel preferences, please try again.");//prints this in case none of the locations match the selected preferences
       else
       {
-        System.out.println("Those are our recommendations for your visit, have fun!!");
+        System.out.println("Those are our recommendations for your visit, have fun!!");//printed after the list of recommended attractions
       }
   }
 }
